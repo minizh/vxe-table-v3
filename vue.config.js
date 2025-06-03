962891d3fe73e5db750a4460173f0aec67c85bb0
minizh <35335674+minizh@users.noreply.github.com>
2025-06-03 16:08:02 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  productionSourceMap: true,
+  configureWebpack: {
+    devtool: "source-map",
+    resolve: {
+      alias: {
+        "@": resolve("src"),
+      },
+    },
+  },
+});
